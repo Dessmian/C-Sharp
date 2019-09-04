4b08cc7c3690f974028ebb3ad7c6a518fa250502
Dessmian <murraydemian@gmail.com>
2019-09-04 11:56:45 -0300

    Se agrego contenido de la clase 06

---
+++ SuarezMurray.Demian/Clase_06.Entidades/Paleta.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Extensions;
+
+
+namespace Clase_06.Entidades
+{
+    class Paleta
+    {
+        private Tempera[] colores;
+        private int cantidadMaximaColores;
+
+        private Paleta():this(5){}
+        private Paleta(int ammnt)
+        {
+            this.cantidadMaximaColores = ammnt;
+            this.colores = new Tempera[this.cantidadMaximaColores];
+        }
+        public static implicit operator Paleta(int ammnt)
+        {
+            return new Paleta(ammnt);
+        }
+        private string Mostrar()
+        {
+            string retString = "nullObject";
+            if (!this.Equals(null))
+            {
+                int i, j;
+                j = this.colores.Count();
+                retString = "Cantidad maxima: " + this.cantidadMaximaColores.ToString();
+                if (this.colores.Count().IsGreaterThan(0))
+                {
+                    retString += " Temperas: ";
+                    for (i=0; i<j;i++)
+                    {
+                        retString += " - ";
+                        retString += this.colores[i];
+                    }
+                }
+            }
+            return retString;
+        }
+        private int ObtenerLugarLibre()
+        {
+            int retIndex = -1;
+            if (!this.Equals(null))
+            {
+                int i;
+                for (i=0;i<this.cantidadMaximaColores;i++)
+                {
+                    if (this.colores[i].Equals(null))
+                    {
+                        retIndex = i;
+                        break;
+                    }
+                }
+            }
+            return retIndex;
+        }
+        public static explicit operator string (Paleta palet)
+        {
+            return palet.Mostrar();
+        }
+        public static bool operator ==(Paleta inPalet, Tempera inTemp)
+        {
+            bool retBool = false;
+            if (!inPalet.Equals(null))
+            {
+                int i;
+                for (i=0;i<inPalet.cantidadMaximaColores;i++)
+                {
+                    if (inPalet.colores[i]==inTemp)
+                    {
+                        retBool = true;
+                        break;
+                    }
+                }
+            }
+            return retBool;
+        }
+        public static bool operator !=(Paleta inPalet, Tempera inTemp)
+        {
+            return !(inPalet == inTemp);
+        }
+        public static Paleta operator +(Paleta inPalet, Tempera inTemp)
+        {
+            if (inPalet == inTemp)
+            {
+                int i;
+                for (i=0;i<inPalet.cantidadMaximaColores;i++)
+                {
+                    if (inPalet.colores[i] == inTemp)
+                    {
+                        inPalet.colores[i] += inTemp;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                int index;
+                index = inPalet.ObtenerLugarLibre();
+                if (index>=0)
+                {
+                    inPalet.colores[index] = inTemp;
+                }
+            }
+            return inPalet;
+        }
+        public static int operator |(Paleta inPalet, Tempera inTemp)
+        {
+            int retInt = -1;
+            if (!inPalet.Equals(null))
+            {
+                int i;
+                for (i=0;i<inPalet.cantidadMaximaColores;i++)
+                {
+                    if (inPalet.colores[i] == inTemp)
+                    {
+                        retInt = i;
+                        break;
+                    }
+                }
+            }
+            return retInt;
+        }
+    }
+}
---
+++ SuarezMurray.Demian/Clase_06.Entidades/Tempera.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_06.Entidades
+{
+    public class Tempera
+    {
+        private ConsoleColor color;
+        private string marca;
+        private int cantidad;
+
+        public Tempera(ConsoleColor clr, string brnd, int ammnt)
+        {
+            this.color = clr;
+            this.marca = brnd;
+            this.cantidad = ammnt;
+        }
+        private string Mostrar()
+        {
+            string retString = "nullObject";
+            if (!this.Equals(null))
+            {
+                retString = this.color.ToString() + " - " + this.marca + " - " + this.cantidad.ToString();
+            }
+            return retString;
+        }
+        public static implicit operator string (Tempera inObject)
+        {
+            return inObject.Mostrar();
+        }
+        public static bool operator ==(Tempera inOne, Tempera inTwo)
+        {
+            bool retBool = false;
+            if (!inOne.Equals(null))
+            {
+                if (!inTwo.Equals(null))
+                {
+                    if (inOne.marca == inTwo.marca)
+                    {
+                        if (inOne.color == inTwo.color)
+                        {
+                            retBool = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (inTwo.Equals(null))
+                {
+                    retBool = true;
+                }
+            }
+            return retBool;
+        }
+        public static bool operator !=(Tempera inOne, Tempera inTwo)
+        {
+            return !(inOne == inTwo);
+        }
+        public static Tempera operator +(Tempera inTemp, int inAmmnt)
+        {
+            if (!inTemp.Equals(null))
+            {
+                inTemp.cantidad += inAmmnt;
+            }
+            return inTemp;
+        }
+        public static Tempera operator +(Tempera inOne, Tempera inTwo)
+        {
+            if (inOne == inTwo)
+            {
+                inOne += inTwo.cantidad;
+            }
+            return inOne;
+        }
+    }
+}
