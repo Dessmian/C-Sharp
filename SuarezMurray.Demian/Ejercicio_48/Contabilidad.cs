92bb5ddb2e2532d47ed48ffc4231c3e3ceb32f6c
murraydemian <alumno@utnfra.com.ar>
2019-10-16 12:04:56 -0300

    clase 16

---
+++ SuarezMurray.Demian/Ejercicio_48/Contabilidad.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_48
+{
+    public class Contabilidad<T,U> where T : Documento where U : Documento, new() 
+    {
+        private List<T> Egresos;
+        private List<U> Ingresos;
+
+        public Contabilidad()
+        {
+            this.Egresos = new List<T>();
+            this.Ingresos = new List<U>();
+        }
+
+        public static Contabilidad<T,U> operator +(Contabilidad<T,U> c, T egreso)
+        {
+            c.Egresos.Add(egreso);
+            return c;
+        }
+        public static Contabilidad<T,U> operator -(Contabilidad<T,U> c, U ingreso)
+        {
+            c.Ingresos.Add(ingreso);
+            return c;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            str.AppendLine("Egresos: ");
+            foreach(T item in this.Egresos)
+            {
+                str.AppendFormat("{0}\n", item.ToString());
+            }
+            str.AppendLine("Ingresos: ");
+            foreach(U item in this.Ingresos)
+            {
+                str.AppendFormat("{0}\n", item.ToString());
+            }
+            return str.ToString();
+        }
+    }
+}
---
+++ SuarezMurray.Demian/Ejercicio_48/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contabilidad<Factura, Recibo> cont = new Contabilidad<Factura, Recibo>();
+
+            Factura f1 = new Factura(10);
+            Recibo r1 = new Recibo(10);
+
+            Console.Write(cont.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
