36190a100bd680978b2b4a66ed022d70ef0fd0ab
murraydemian <murraydemian@gmail.com>
2019-11-26 19:31:02 -0300

    update

---
+++ MockUp_2doParcial/MockUp_2doParcial/SQLMockUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace MockUp_2doParcial
+{
+    public static class SQLMockUp
+    {
+        private static SqlConnection connection;
+        private static SqlCommand comando;
+
+        static SQLMockUp()
+        {
+            SqlConnection conexion = new SqlConnection("Server=.\\SQLEXPRESS;Database=<Nombre del DB>;Trusted_Connection=True;");
+            SQLMockUp.connection = conexion;
+        }        
+        public static void Conectar()
+        {
+            try
+            {
+                SQLMockUp.connection.Open();
+                //Hacer algo
+            }
+            finally
+            {
+                if(SQLMockUp.connection.State == System.Data.ConnectionState.Open)
+                    SQLMockUp.connection.Close();
+            }
+        }
+        public static void CrearComando()
+        {
+            SqlCommand traer = new SqlCommand("SELECT * FROM <DB>", SQLMockUp.connection);
+
+            SqlCommand insertar = new SqlCommand("INSERT INTO <DB> VALUES (@v1,@v2,@v3)", SQLMockUp.connection);
+            insertar.Parameters.AddWithValue("@v1", "Valor 1");
+            insertar.Parameters.AddWithValue("@v2", "Valor 2");
+            insertar.Parameters.AddWithValue("@v3", "Valor 3");
+
+            SqlCommand actualizar = new SqlCommand("UPDATE <DB> SET campo = @valor WHERE id = @valor", SQLMockUp.connection);
+
+            SqlCommand borrar = new SqlCommand("DELETE FROM <DB> WHERE id = @valor");
+        }
+    }
+}
---
+++ MockUp_2doParcial/MockUp_2doParcial/SerializacionXML.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace MockUp_2doParcial
+{
+    public class SerializacionXML<T>
+    {
+        public static void Serializar(T item, string path)
+        {
+            //El streamWriter se encarga de escribir el archivo.
+            StreamWriter writer = new StreamWriter(path);
+            //El serializador se encarga de formatear los datos a guardar.
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            //El metodo serialize necesita al encargado de escribir el archivo y lo que se necesita guardar.
+            serializer.Serialize(writer, item);
+        }
+        public static void Deserializar(out T item, string path)
+        {
+            StreamReader reader = new StreamReader(path);
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            item = (T)serializer.Deserialize(reader);
+        }
+    }
+}
